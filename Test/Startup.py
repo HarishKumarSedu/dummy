455f1548338eda8d0c5457f157cd5eaa2a1db2bb
Harish Kumar Shivaramappa <110411906+HarishKumarSedu@users.noreply.github.com>
2025-11-14 16:32:55 +0100

    Add/update multiple files commit

---
+++ Test/Startup.py
@@ -0,0 +1,9 @@
+import os 
+import sys
+# example code to install python pacakage
+# command = f"{sys.executable} -m pip uninstall dfttools -y"
+# os.system(command)
+# command = f"{sys.executable} -m pip install git+https://github.com/HarishKumarSedu/dfttools.git@main"
+# os.system(command)
+
+print('hi')
